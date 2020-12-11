1698e03821cc9fda944d46c67e316dac2cf38719
David Sarif <you@example.comDavidSarif@yandex.ru>
2020-12-12 01:57:48 +0300

    fix: fix grid layout due to container component rendering

---
+++ src/components/Map/MapContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Map from './Map'
+
+const MapContainer = (props:any) => {
+    return (
+        <Map className={props.className}/>
+    )
+}
+
+export default MapContainer
