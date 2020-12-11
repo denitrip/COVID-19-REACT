bb1ac359efc299578809c5068d9d9dce6df7a3f2
David Sarif <you@example.comDavidSarif@yandex.ru>
2020-12-12 01:39:27 +0300

    quickfix: now app renders container components instead of presentational ones

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@
 import React from 'react';
 import s from'./App.module.css';
 import Header from './components/Header/Header';
-import Search from './components/Search/Search';
-import MainList from './components/MainList/MainList';
-import Map from './components/Map/Map';
-import MainTable from './components/MainTable/MainTable';
-import Graph from './components/Graph/Graph';
+import SearchContainer from './components/Search/Search';
+import MainListContainer from './components/MainList/MainList';
+import MapContainer from './components/Map/Map';
+import MainTableContainer from './components/MainTable/MainTable';
+import GraphContainer from './components/Graph/Graph';
 
 function App() {
   return (
     <div className={s.app}>
       <Header className={s.header}/>
-      <Search className={s.search}/>
-      <MainList className={s.mainList}/>
-      <Map className={s.map}/>
-      <MainTable className={s.mainTable}/>
-      <Graph className={s.graph}/>
+      <SearchContainer className={s.search}/>
+      <MainListContainer className={s.mainList}/>
+      <MapContainer className={s.map}/>
+      <MainTableContainer className={s.mainTable}/>
+      <GraphContainer className={s.graph}/>
     </div>
   );
 }
