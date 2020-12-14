070a7498b4c5bb544596988bfeb0bb2fab3a9a00
cup0ra <cup0ra@users.noreply.github.com>
2020-12-14 20:30:08 +0300

    fix: change App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
-import GraphContainer from "./components/Graph/GraphContainer";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <GraphContainer />
     </div>
   );
 }
