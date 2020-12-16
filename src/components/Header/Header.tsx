8fd98015fa371f874a42d04d518a723e67851963
Ritaheav <rita20071995@hotmail.com>
2020-12-17 01:37:13 +0300

    feat: add header and footer block

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import style from "./Header.module.css";
 
 const Header = (props:any) => {
     return(
-        <div className={props.className}>HEADER</div>
+        <div className={[props.className, style.header_block].join(' ')}>
+            <h1 className={style.header_name}>COVID-19 Dashboard</h1>
+        </div>
     )
 }
 
