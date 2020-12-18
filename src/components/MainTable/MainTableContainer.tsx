677203fe647dd75bdf7deb297b4fb0bb89d26009
David Sarif <you@example.comDavidSarif@yandex.ru>
2020-12-19 01:38:16 +0300

    feat:now mainTable is rendering real data and syncronized with rest of the app

--- src/components/MainTable/MainTableContainer.tsx
+++ src/components/MainTable/MainTableContainer.tsx
@@ -1,9 +1,45 @@
 import React, { useEffect, useState } from "react";
 import MainTable from "./MainTable";
 import { IdataField } from "../../model/main-table.model";
+import { Spinner } from "../Spinner/Spinner";
+import { GLOBAL_POPULATION } from "../../constants";
+import s from "./MainTable.module.scss";
+import defaultFlag from '../../assets/image/640px-International_Flag_of_Planet_Earth.png'
 
 const MainTableContainer = (props: any) => {
   let [checked, setChecked] = useState<boolean>(false);
+  useEffect(() => props.updateCheckAbsolut(checked), [checked]);
+  useEffect(() => setChecked(props.checkAbsolut), [props.checkAbsolut]);
+  const globalPopulation: number = GLOBAL_POPULATION;
+  let countryFlag = '';
+  let population: number = undefined;
+  let rawData = undefined;
+  let country:string = "";
+  
+  // if no data ready for component
+  if (!props.data) {
+    return (
+      <div className={s.wrapper}>
+        <Spinner />
+      </div>
+    );
+  }
+  // if we are using global data
+  if (!props.countryObj) {
+    rawData = props.data.Global;
+    country = "Global";
+    population = globalPopulation;
+    countryFlag = defaultFlag
+  } else {
+    population = props.countryObj.population;
+    let countryCode = props.countryObj.country;
+    rawData = props.data.Countries.filter((el) => {
+      return el.alpha2Code === countryCode;
+    }).pop();
+    country = rawData.Country;
+    countryFlag = rawData.flag
+  }
+
   let fieldsForData = [
     "NewConfirmed",
     "NewDeaths",
@@ -12,49 +48,26 @@ const MainTableContainer = (props: any) => {
     "TotalDeaths",
     "TotalRecovered",
   ];
-  let fakeData = {
-    Country: "Russian Federation",
-    CountryCode: "RU",
-    Date: "2020-12-14T14:37:49Z",
-    NewConfirmed: 27651,
-    NewDeaths: 481,
-    NewRecovered: 20177,
-    Premium: {},
-    Slug: "russia",
-    TotalConfirmed: 2629699,
-    TotalDeaths: 46404,
-    TotalRecovered: 2086887,
-  };
-  let data = Object.entries(fakeData)
+  let data = Object.entries(rawData)
     .map(([key, value]) => {
       return { title: key.toString(), count: +value };
     })
     .filter((el) => {
       return fieldsForData.includes(el.title);
     });
-
-  const country = fakeData.Country;
-  const sortedData: Array<IdataField> = [
-    data[3],
-    data[0],
-    data[4],
-    data[1],
-    data[5],
-    data[2],
-  ];
   const switchData = { onSwitchChange: setChecked, switchChecked: checked };
-  useEffect(() => props.updateCheckAbsolut(checked), [checked]);
-  useEffect(() => setChecked(props.checkAbsolut), [props.checkAbsolut]);
+
   if (checked) {
     for (let elem of data) {
-      elem.count = Number((elem.count / 100000).toFixed(3));
+      elem.count = Number(((elem.count / population) * 100000).toFixed(3));
     }
   }
   return (
     <MainTable
-      payload={{ sortedData, country }}
+      payload={{ data, country }}
       className={props.className}
       switchData={switchData}
+      countryFlag={countryFlag}
     />
   );
 };
