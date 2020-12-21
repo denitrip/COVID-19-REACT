5bd66c69bc8f1201f17f90ea8f49d42a10694f9c
cup0ra <cup0ra@users.noreply.github.com>
2020-12-21 16:18:39 +0300

    feat: added search country

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
+import style from "./Search.module.scss";
 
-const Search = (props:any) => {
+const Search = (props) => {
+  const dataSearch = (e) => {
+    const value = e.target.value.toLowerCase();
+    const filter = props.data.Countries.filter((item) => {
+      return item.Country.toLowerCase().slice(0, value.length) === value;
+    });
+    props.updateDataSearch(filter);
+  };
+  return (
+    <div className={props.className}>
+      <div className={style.container}>
+        <div className={style.wrapper}>
+          <input
+            type="text"
+            className={style.input}
+            placeholder="Search country..."
+            onChange={dataSearch}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    return(
-        <div className={props.className}>Search</div>
-    )
-}
-
-export default Search
+export default Search;
